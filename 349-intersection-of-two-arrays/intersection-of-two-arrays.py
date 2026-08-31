082c8f38739e78ee6238469e04954955196a036a
Akhil Reddy <akhilreddypoola07@gmail.com>
2026-08-31 20:27:17 +0530

    Time: 0 ms (100.00%) | Memory: 12.6 MB (30.48%) - LeetSync

--- 349-intersection-of-two-arrays/intersection-of-two-arrays.py
+++ 349-intersection-of-two-arrays/intersection-of-two-arrays.py
@@ -1,9 +1,8 @@
-class Solution:
-    def intersection(self, nums1: List[int], nums2: List[int]) -> List[int]:
-        result = []
-        for i in nums1:
-            for j in nums2:
-                if i == j and i not in result:
-                    result.append(i)
-        return result
- 
+class Solution(object):
+    def intersection(self, nums1, nums2):
+        """
+        :type nums1: List[int]
+        :type nums2: List[int]
+        :rtype: List[int]
+        """
+        return list(set(nums1) & set(nums2))
